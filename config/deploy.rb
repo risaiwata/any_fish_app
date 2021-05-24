6db2970acb254208c99817fc0a99c119a7e60809
risaiwata <82008655+risaiwata@users.noreply.github.com>
2021-05-24 21:29:10 +0900

    ユニコーン修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.1'
+lock '3.16.0'
 
 # Capistranoのログの表示に利用する
 set :application, 'anyfishapp'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:risaiwata/ユニコーンの修正.git'
+set :repo_url,  'git@github.com:risaiwata/any_fish_app.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
