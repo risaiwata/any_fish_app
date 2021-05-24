4a748f97aa434989b7d4e81f189c9018ae4bae0f
risaiwata <82008655+risaiwata@users.noreply.github.com>
2021-05-24 20:32:26 +0900

    add imageのmg 削除

--- db/migrate/20210517064510_add_image_to_agents.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToAgents < ActiveRecord::Migration[6.0]
-  def change
-    add_column :agents, :image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -69,12 +69,12 @@ ActiveRecord::Schema.define(version: 2021_05_19_052310) do
     t.integer "fishing_type_id", null: false
     t.text "profile"
     t.text "sheet", null: false
+    t.string "image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
     t.index ["email"], name: "index_agents_on_email", unique: true
     t.index ["reset_password_token"], name: "index_agents_on_reset_password_token", unique: true
   end
